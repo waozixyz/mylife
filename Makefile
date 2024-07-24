f1a9500bb3958362885d65ed5e0bbe3ef5d75f54
M. Schiller <waotzi@proton.me>
2024-07-24 21:32:18 +0700

    add devenv

--- Makefile
+++ Makefile
@@ -4,20 +4,40 @@ WRAPPER_DIR = $(HOME)/bin
 WRAPPER_PATH = $(WRAPPER_DIR)/$(APP_NAME)
 NIX_SHELL = nix-shell
 SHELL_NIX = shell.nix
+CC = gcc
+CFLAGS = -Wall -Wextra -pedantic
+LIBS = -lraylib -lm -lpthread -ldl -lrt
 
-.PHONY: all install clean
+.PHONY: all install clean tcl c
 
-all: install
+all: tcl c
 
-install: $(WRAPPER_PATH)
+tcl: $(WRAPPER_PATH)
+
+c: $(APP_NAME)_c
+
+install: tcl c
 
 $(WRAPPER_PATH): $(SHELL_NIX) life.tcl config.yaml
 	mkdir -p $(WRAPPER_DIR)
 	echo "#!/bin/sh" > $(WRAPPER_PATH)
 	echo "$(NIX_SHELL) $(SHELL_NIX) --run 'tclsh life.tcl'" >> $(WRAPPER_PATH)
 	chmod +x $(WRAPPER_PATH)
-	@echo "Wrapper script installed at $(WRAPPER_PATH)"
+	@echo "Tcl wrapper script installed at $(WRAPPER_PATH)"
+
+$(APP_NAME)_c: life.c
+	$(CC) $(CFLAGS) -o $@ $< $(LIBS)
+	@echo "C version compiled as $(APP_NAME)_c"
 
 clean:
 	rm -f $(WRAPPER_PATH)
+	rm -f $(APP_NAME)_c
 	@echo "Wrapper script removed from $(WRAPPER_PATH)"
+	@echo "C executable removed"
+
+run_tcl:
+	$(NIX_SHELL) $(SHELL_NIX) --run 'tclsh life.tcl'
+
+run_c: $(APP_NAME)_c
+	./$(APP_NAME)_c
+	
--- devenv.nix
+++ devenv.nix
@@ -1,52 +1,28 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs, ... }:
 
-let
-  # Custom script to build and run the C version
-  buildRunC = pkgs.writeScriptBin "build-run-c" ''
-    #!/bin/sh
-    cc -o life_c life.c -lraylib -lm -lpthread -ldl -lrt
-    ./life_c
-  '';
-
-  # Custom script to run the Tcl version
-  runTcl = pkgs.writeScriptBin "run-tcl" ''
-    #!/bin/sh
-    tclsh life.tcl
-  '';
-
-  # Custom script to serve the web version
-  serveWeb = pkgs.writeScriptBin "serve-web" ''
-    #!/bin/sh
-    cd html
-    ${pkgs.python3}/bin/python -m http.server 8000
-  '';
-
-in pkgs.mkShell {
-  buildInputs = [
-    # Tcl dependencies
+{
+  packages = [
     pkgs.tcl
     pkgs.tk
     pkgs.tcllib
-
-    # C and Raylib dependencies
     pkgs.gcc
     pkgs.raylib
     pkgs.cmake
     pkgs.pkg-config
-
-    # Web dependencies
     pkgs.python3
-
-    # Custom scripts
-    buildRunC
-    runTcl
-    serveWeb
   ];
 
-  shellHook = ''
+  scripts = {
+    build-run-c.exec = "cc -o life_c life.c -lraylib -lm -lpthread -ldl -lrt && ./life_c";
+    run-tcl.exec = "tclsh life.tcl";
+    serve-web.exec = "cd html && python -m http.server 8000";
+  };
+
+  enterShell = ''
     echo "Environment set up. You can use the following commands:"
     echo "  build-run-c  : Build and run the C version"
     echo "  run-tcl      : Run the Tcl version"
     echo "  serve-web    : Serve the web version (accessible at http://localhost:8000)"
+ 
   '';
 }
